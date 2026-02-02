1ce0512471bb29545963066cf5b9c493c4a9642b
Engr. UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2026-02-02 15:59:54 +0500

    Create index.js

---
+++ SecondTask/backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "A joke",
+      content: "This is a joke",
+    },
+    {
+      id: 2,
+      title: "2nd joke",
+      content: "This is 2nd joke",
+    },
+    {
+      id: 3,
+      title: "3rd joke",
+      content: "This is 3rd joke",
+    },
+    {
+      id: 4,
+      title: "4yh joke",
+      content: "This is 4th joke",
+    },
+    {
+      id: 5,
+      title: "5th joke",
+      content: "This is 5th joke",
+    },
+  ];
+  res.send(jokes);
+});
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
